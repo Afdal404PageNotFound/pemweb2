544607229dbce8c66a459fd491625d082fb8dfbf
honyopenyoko <comboafdal@gmail.com>
2024-02-24 08:11:10 +0700

    pemweb2

---
+++ array_assosiatif.php
@@ -0,0 +1,9 @@
+<?php
+
+$mhs1 = ['nama' => 'Muhammad afdal al ghazali', 
+'nim' => '0110123279', 
+"Jurusan" => 'Sistem Informasi',
+];
+
+// Memanggil array assosiatif
+echo 'Nama: ' . $mhs1['nama'];
